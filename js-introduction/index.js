48c9a19cbc9db6611b8071d71181169f816ee31e
mrstraw-hat <142039478+mrstraw-hat@users.noreply.github.com>
2023-08-15 08:50:50 -0700

    Add files via upload

---
+++ js-introduction/index.js
@@ -0,0 +1,29 @@
+// printing something to console
+// console.log("hello world")
+ alert("GET LOST")
+// let my_word = "hello";
+//  console.log(my_word)
+// var my_word = "hello";
+// let a = "a";
+// a = "b";
+// console.log(a);
+// const HELLO = "HI";
+const HELLO = "boo";
+console.log(HELLO);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
