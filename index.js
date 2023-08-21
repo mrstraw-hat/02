646133e6f7763e04c6c5b58b2e6ab31cc98b189f
mr.beluga <@muhammedfalah90>
2023-08-21 08:17:06 -0700

    more things

--- index.js
+++ index.js
@@ -33,12 +33,33 @@
 
 // BOOLIAN
 
-let a = true;
-let b = false;
-console.log(typeof(a));
+// let a = true;
+// let b = false;
+// console.log(typeof(a));
+
+
+// ARRAY also means object but array is a different object not like null (null is also an object)
+// array means more than one word is arranged and stored
+
+let mylist = [1,2,3];
+// let items = ["Hello world", "Get lost"];
+// let mix = ["Hello world", "Get lost", 786];
+// positions = [0, 1, 2] position is the index means positioning .positioning starts at 0 not at 1
+let students = ["appu", "john","jane","zom","bash"]
+console.log(students);
+// students.push("matt") by using these you can addmore content to the last of the content line 
+// students.pop(); this thing is exactly like the push but removesinsted of adding
+// students.push("zom")
+// students[3] = "matt";
+// students[5] = "zom";
+// students.shift(); shift removes the content frome the first insted of the last
+students.splice(2, 2)
+console.log(students);
 
 
 
+
+// console.log(students[2]) by using this it will tell you waht is thecontent in the index position 2 wchich means the thirdone beacuse indexstarts frome0 not 1
 
 
 
